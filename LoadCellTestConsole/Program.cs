68596e72b78f9775a175e5cf5b863ab9c2c8e77f
alimutluebt <ali.mutlu@egebimtes.com.tr>
2020-02-12 16:17:58 +0300

    -Levent Kimya Load Cell Test Çalışması

---
+++ LoadCellTestConsole/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+
+namespace LoadCellTestConsole {
+    class Program {
+        static void Main(string[] args) {
+
+            Connect("192.168.18.201", 502);
+
+            Console.Read();
+        }
+
+        public static void Connect(string host, int port)
+        {
+            IPAddress[] IPs = Dns.GetHostAddresses(host);
+
+            Socket s = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+
+            Console.WriteLine("Establishing Connection to {0}",host);
+            s.Connect(IPs[0], port);
+
+            byte[] howdyBytes = Encoding.ASCII.GetBytes("Howdy");
+            s.Send(howdyBytes);
+            byte[] buffer = new byte[50];
+            s.Receive(buffer);
+            string replacedData = Encoding.UTF8.GetString(buffer).Replace("z00", "");
+            replacedData = replacedData.Substring(1,8);
+            replacedData = replacedData.Trim( new Char[] {' '} );
+            Console.Write("Load Cell Okunan Değer: "+ replacedData);
+            
+        }
+    }
+}
